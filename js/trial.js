222dab467de4bf3fc339b092c97d0b2ce0272ca4
alari <name.alari@gmail.com>
2017-09-24 19:58:36 +0300

    multiple patients

--- js/trial.js
+++ js/trial.js
@@ -1,37 +1,36 @@
 require('babel-register');
 require('babel-polyfill');
 
-var Web3 = require('web3');
-var fs = require('fs');
-var Q = require('q');
-var moment = require('moment');
-var ipfs = require('ipfs');
+const Web3 = require('web3');
+const fs = require('fs');
+const Q = require('q');
+const moment = require('moment');
+const ipfs = require('ipfs');
 
-var ipfsNode = new ipfs();
+const ipfsNode = new ipfs();
 
-var web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8547'));
+const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8547'));
 
-var trialBin = fs.readFileSync('./contract.bin', 'utf8');
-var trialAbi = require('./contract-abi.json');
+const trialBin = fs.readFileSync('./contract.bin', 'utf8');
+const trialAbi = require('./contract-abi.json');
 
-var trialContractAddress = '0x6aeade68950e94107bad70a3478de44987bf638b';
-var trialContract = web3.eth.contract(trialAbi);
+var trialContractAddress;
+const trialContract = web3.eth.contract(trialAbi);
 
-var defaultGas = 4700000;
+const defaultGas = 4700000;
 
-var pharmAccount = '0x7125e93d19079c4174473651681a072311000415';
-var researcherAccount = '0x0ed09b0147820eda3df5c736330a86a1d5378d18';
-var patientAccount1 = '0xa97720dcd6ac4e5ffbc85744d9677cce1fa73126';
+const pharmAccount = '0x7125e93d19079c4174473651681a072311000415';
+const researcherAccount = '0x0ed09b0147820eda3df5c736330a86a1d5378d18';
 
-var _bigPharma = pharmAccount;
-var _numOfPatients = 1;
-var _researcher = researcherAccount;
+var numOfPatients;
 
-var mappingHash = '123';
-var entryHash = 'qwe';
-var entryHashFinal = 'qwe2';
-var metric = 10;
-var isPlacebo = true;
+const patients = [
+  '0xa97720dcd6ac4e5ffbc85744d9677cce1fa73126',
+  '0xee8fc1b17c98de61ab04e080c41813fa0a27c583',
+  '0x38e1dd8c8e36138c15483fa7a897f8e00b6ec87a',
+  '0x10696a3024ce6fc1024e18163fe97914e9d58d99',
+  '0xb891ff0adf5fd40223ce7c94c6ef1b6d77c89b63'
+]
 
 
 console.log("Connecting to node");
@@ -41,25 +40,15 @@ web3.eth.getBlockNumber(function(error, number){
    console.log("blockNumber= "+number);
 });
 
-function hex2string(hex) {
-   hex = hex.slice(2);
-   const str = new Buffer(hex, 'hex').toString();
-   var ret= "";
-   for(var i=0;i<str.length;i++) {
-      if(str[i] == String.fromCharCode(0)) { break; }
-      ret += str[i];
-   }
-   return ret;
-}
-
 function runPromise(cb) {
   return new Promise((resolve, reject) => cb((err, res) =>
     !!err ? reject(err) : resolve(res)
   ))
 }
 
-async function deployTrialContract() {
-  return runPromise(cb => trialContract.new(_bigPharma, _numOfPatients, _researcher,
+async function deployTrialContract(_numOfPatients) {
+  numOfPatients = _numOfPatients;
+  return runPromise(cb => trialContract.new(pharmAccount, _numOfPatients, researcherAccount,
     { from: pharmAccount, data: trialBin, gas: defaultGas }, (err, contract) => {
     if(!contract.address) return;
     if(!!contract && !!contract.address) trialContractAddress = contract.address
@@ -68,9 +57,12 @@ async function deployTrialContract() {
 }
 
 async function registerPatient() {
-  return runPromise(cb =>
+  console.log("register patient", patients.slice(0, numOfPatients))
+
+  return Promise.all(patients.slice(0, numOfPatients).map(p =>  runPromise(cb =>
     trialContract.at(trialContractAddress)
-      .registerPatient.sendTransaction({from: patientAccount1, gas: defaultGas},cb))
+      .registerPatient.sendTransaction({from: p, gas: defaultGas},cb))))
+
 }
 
 async function setPlaceboEncryptedMappingHash(_hash) {
@@ -80,31 +72,31 @@ async function setPlaceboEncryptedMappingHash(_hash) {
 }
 
 async function recordEntry(_entryHash) {
-  return runPromise(cb =>
+  return Promise.all(patients.slice(0, numOfPatients).map((p, i) =>  runPromise(cb =>
     trialContract.at(trialContractAddress)
-      .recordEntry.sendTransaction(_entryHash, {from: patientAccount1, gas: defaultGas}, cb)
-  )
+      .recordEntry.sendTransaction(_entryHash[i], {from: p, gas: defaultGas}, cb)
+  )))
 }
 
 async function recordFinal(_hash) {
-  return runPromise(cb =>
+  return Promise.all(patients.slice(0, numOfPatients).map((p, i) =>  runPromise(cb =>
     trialContract.at(trialContractAddress)
-      .recordFinal.sendTransaction(_hash, {from: patientAccount1, gas: defaultGas}, cb)
-  )
+      .recordFinal.sendTransaction(_hash[i], {from: p, gas: defaultGas}, cb)
+  )))
 }
 
 async function recordMetric(_metric) {
-  return runPromise(cb =>
+  return Promise.all(patients.slice(0, numOfPatients).map((p, i) =>  runPromise(cb =>
     trialContract.at(trialContractAddress)
-      .recordMetric.sendTransaction(patientAccount1, _metric, {from: researcherAccount, gas: defaultGas}, cb)
-  )
+      .recordMetric.sendTransaction(p, _metric[i], {from: researcherAccount, gas: defaultGas}, cb)
+  )))
 }
 
 async function revealPlaceboOrPill(_isPlacebo) {
-  return runPromise(cb =>
+  return Promise.all(patients.slice(0, numOfPatients).map((p, i) =>  runPromise(cb =>
     trialContract.at(trialContractAddress)
-      .revealPlaceboOrPill.sendTransaction(patientAccount1, _isPlacebo, {from: pharmAccount, gas: defaultGas}, cb)
-  )
+      .revealPlaceboOrPill.sendTransaction(p, _isPlacebo[i], {from: pharmAccount, gas: defaultGas}, cb)
+  )))
 }
 
 async function readResults() {
@@ -159,16 +151,16 @@ function main() {
 async function trial(){
 
 
-  const contract = await deployTrialContract();
+  const contract = await deployTrialContract(1);
   console.log("mined: ", contract.address)
 
   const data = fs.readFileSync('./data.txt', 'utf8').split('\n');
 
   const mappingHash = data[0]
-  const entryHash = data[1]
-  const finalHash = data[2]
-  const metric = parseInt(data[3])
-  const isPlacebo = data[4].toLowerCase() === "true"
+  const entryHash = data[1].split('|')
+  const finalHash = data[2].split('|')
+  const metric = data[3].split('|').map(s => parseInt(s))
+  const isPlacebo = data[4].split('|').map(ip => ip.toLowerCase() === "true")
 
   // console.log("mappingHash", mappingHash)
   // console.log("entryHash", entryHash)
